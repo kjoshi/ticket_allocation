48420698dcad91dc3435f949371eea7a42c99643
Kiran Joshi <kiran.j88@gmail.com>
2025-03-25 14:19:34 +0000

    Add HTTP server and dummy handler for index

--- api/main.go
+++ api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	// "tickets/models"
+)
+
+type application struct{}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	port := getEnv("PORT", "5555")
+
+	app := &application{}
+
+	log.Printf("Server starting on port %s", port)
+	err := http.ListenAndServe(":"+port, app.routes())
+	log.Fatal(err.Error())
+	os.Exit(1)
+
 }
