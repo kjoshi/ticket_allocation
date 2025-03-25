3bdc4ed255d0ad7bd924cef3979b626233dc892b
Kiran Joshi <kiran.j88@gmail.com>
2025-03-25 14:03:09 +0000

    Add data models

---
+++ api/models/http.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type CreateTicketOptionRequest struct {
+	Data struct {
+		Type       string `json:"type"`
+		Attributes struct {
+			Name        string `json:"name"`
+			Description string `json:"description"`
+			Allocation  int    `json:"allocation"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
+
+type TicketOptionResponse struct {
+	Data struct {
+		Type       string    `json:"type"`
+		ID         uuid.UUID `json:"id"`
+		Attributes struct {
+			Name        string `json:"name"`
+			Description string `json:"description"`
+			Allocation  int    `json:"allocation"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
+
+type CreatePurchaseRequest struct {
+	Data struct {
+		Type       string `json:"type"`
+		Attributes struct {
+			Quantity int `json:"quantity"`
+		} `json:"attributes"`
+		Relationships struct {
+			TicketOption struct {
+				Data struct {
+					Type string    `json:"type"`
+					ID   uuid.UUID `json:"id"`
+				} `json:"data"`
+			} `json:"ticket_option"`
+			User struct {
+				Data struct {
+					Type string    `json:"type"`
+					ID   uuid.UUID `json:"id"`
+				} `json:"data"`
+			} `json:"user"`
+		} `json:"relationships"`
+	} `json:"data"`
+}
